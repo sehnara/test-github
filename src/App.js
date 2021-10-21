d35406f86f8ac584f47cce2f7b99d8c144d0079b
sehoon <gsh723@naver.com>
2021-10-21 10:53:05 +0900

    test 3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edizt <code>src/Apdp.js</code> and save to reload.
+          Edizt <code>src/Apdgitp.js</code> and save to reload.
         </p>
         <a
           className="App-link"
